726cc1834d5ecf70b7b37b30d0cd9dbff418f02e
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-09-04 12:35:39 -0700

    add: modal for selecting themes

--- test-app-1/src/App.js
+++ test-app-1/src/App.js
@@ -1,22 +1,99 @@
-import './App.css';
+import { useState, useEffect } from 'react';
 import { quotes } from './services/data-svc';
+import './App.css';
+
 import { ReactComponent as PaletteIcon } from './palette.svg';
 import { ReactComponent as RestartIcon } from './restart.svg';
 
-const randomQuote = quotes[4].text;
+function pickRandomeQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const item = quotes[randomIndex];
+  return item;
+}
+
+const themes = [
+  '8008',
+  'tron',
+  'check',
+  'flex',
+  '8008',
+  'tron',
+  'check',
+  'flex',
+  '8008',
+  'tron',
+  'check',
+  'flex',
+  '8008',
+  'tron',
+  'check',
+  'flex',
+  '8008',
+  'tron',
+  'check',
+  'flex',
+  '8008',
+  'tron',
+  'check',
+  'flex',
+  '8008',
+  'tron',
+  'check',
+  'flex',
+  '8008',
+  'tron',
+  'check',
+  'flex',
+  '8008',
+  'tron',
+  'check',
+  'flex',
+  '8008',
+  'tron',
+  'check',
+  'flex',
+  '8008',
+  'tron',
+  'check',
+  'flex',
+  '8008',
+  'tron',
+  'check',
+  'flex',
+];
+function ThemeList() {
+  return (
+    <div className='wrapper'>
+      <div className='themeListContainer'>
+        <div className='themeList'>
+          {themes.map((item) => (
+            <div className='theme'>{item}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  const [quote, setQuote] = useState(pickRandomeQuote().text);
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => setModal(!modal);
+
   return (
     <div className='container'>
       <div>
-        <div className='currentTheme'>
-          <PaletteIcon /> <span>Tron Orange</span>
+        <div className='currentTheme' onClick={toggleModal}>
+          <PaletteIcon />
+          <span>Tron Orange</span>
+          {modal && <ThemeList />}
         </div>
         <article>
-          <p>{randomQuote}</p>
+          <p>{quote}</p>
         </article>
         <div>
-          <RestartIcon />
+          <RestartIcon onClick={() => setQuote(pickRandomeQuote().text)} />
         </div>
       </div>
     </div>
@@ -24,22 +101,3 @@ function App() {
 }
 
 export default App;
-
-/*
-<div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-        themeing commit
-      </header>
-    </div> */
