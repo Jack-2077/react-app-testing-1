ebf97d452e71359de5b00c1075e734528f32678b
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-09-11 21:00:22 -0700

    test: Theme rendering modal on click

--- test-app-1/src/__tests__/Theme.test.js
+++ test-app-1/src/__tests__/Theme.test.js
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Theme } from '../components';
 
-test('clicking the div opens modal', () => {
+test('render modal', () => {
   const props = {
     handleModal: jest.fn(),
     handleTheme: jest.fn(),
@@ -15,29 +15,3 @@ test('clicking the div opens modal', () => {
   fireEvent.click(currentThemeDiv);
   expect(screen.getByRole('list')).toBeInTheDocument();
 });
-
-/*
-// import { render, screen } from '@testing-library/react';
-// import ThemeList from './ThemeList';
-
-// test('Handles onMouseOve and onClick', () => {
-//   render(<ThemeList />);
-//   const linkElement = screen.getByRole('themeList');
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-
-import { fireEvent, render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders app content', () => {
-  render(<App />);
-  // const onClick = jest.fn();
-  // const restartIconElement = screen.getByTestId('get-new-quote');
-  // fireEvent.click(restartIconElement);
-  // expect(onClick).toHaveBeenCalledTimes(1);
-  // const articleElement = screen.getByRole('textContent');
-  // expect(articleElement).toBeInTheDocument();
-});
-
-*/
