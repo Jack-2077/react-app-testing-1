fb141bb83bc80dc34237ab0c95fbcdc226992be8
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-09-11 21:07:22 -0700

    test: ThemeList close modal on pressing escape key

--- test-app-1/src/__tests__/ThemeList.test.js
+++ test-app-1/src/__tests__/ThemeList.test.js
@@ -24,3 +24,15 @@ test('close modal when a theme is clicked', () => {
   fireEvent.click(screen.getByText('my_theme'));
   expect(changeTheme).toHaveBeenCalledTimes(1);
 });
+
+test('close modal when esc key is pressed', () => {
+  render(<ThemeList {...props} />);
+
+  fireEvent.keyDown(screen.getByTestId('outside-modal'), {
+    key: 'Escape',
+    code: 'Escape',
+    keyCode: 27,
+    charCode: 27,
+  });
+  expect(closeModal).toHaveBeenCalledTimes(1);
+});
